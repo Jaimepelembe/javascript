f04c71b8bf84b79d09b2bb42162a8d86be083eb5
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2023-12-09 14:02:18 +0200

    I created the first scripts for the validation of name, price of factory, tax  percentage and dealer percentage

--- projects/1. price-car/script.js
+++ projects/1. price-car/script.js
@@ -1,6 +1,8 @@
 /*Declaration of variables */
 var outputCarName = document.getElementById("outputCarName");
 var outputCarPrice = document.getElementById("outputCarPrice");
+const buttonCalculate = document.querySelector("#buttonCalculate");
+var nameAlert = document.getElementById("nameAlert");
 
 function setCarName() {
   var carName = document.getElementById("carName");
@@ -54,6 +56,98 @@ function finalPrice() {
 }
 
 function calculate() {
-  setCarName();
-  outputCarPrice.innerHTML = finalPrice();
+  if (
+    validateName() &&
+    validateFactoryPrice() &&
+    validadeTaxPercentage() &&
+    validadeDealerPercentage()
+  ) {
+    setCarName();
+    outputCarPrice.innerHTML = finalPrice();
+  } else {
+    outputCarName.innerHTML = "";
+    outputCarPrice.innerHTML = "";
+  }
+}
+/*
+buttonCalculate.addEventListener("click", (event) => {
+  event.preventDefault();
+});*/
+
+//Verify if the name is valid
+function validateName() {
+  var carName = document.getElementById("carName");
+
+  if (carName.value === "") {
+    //alert("O nome não pode estar vázio");
+    nameAlert.innerHTML = "O nome não pode estar vázio";
+    return false;
+  }
+  var size = carName.value.length;
+  if (size < 3) {
+    //alert(`O nome deve ter pelo menos 3 caracteres, você inseriu ${size}`);
+    nameAlert.innerHTML = `O nome deve ter pelo menos 3 caracteres, você inseriu ${size}`;
+    return false;
+  }
+  nameAlert.innerHTML = "";
+  return true;
+}
+
+//Validate factory price
+function validateFactoryPrice() {
+  var factoryPrice = document.getElementById("factoryPrice").value;
+  factoryPrice = Number.parseFloat(factoryPrice);
+
+  if (isNaN(factoryPrice)) {
+    alert("O preço não pode estar vázio!");
+    return false;
+  } else {
+    if (factoryPrice < 0) {
+      alert("O preço do carro não pode ser negativo!");
+      return false;
+    } else {
+      if (factoryPrice < 100000) {
+        alert("O preço do carro deve ser superior a 100.000,00 mts");
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+//Validate tax percentage
+function validadeTaxPercentage() {
+  var taxPercentage = document.getElementById("taxPercentage").value;
+  taxPercentage = Number.parseFloat(taxPercentage);
+  if (taxPercentage <= 20) {
+    alert("A percentagem de imposto não pode ser inferior a 20%");
+    return false;
+  } else {
+    if (taxPercentage >= 60) {
+      alert("A percentagem de imposto não pode ser superior a 60%");
+      return false;
+    }
+  }
+  return true;
+}
+
+//Validate tax percentage
+function validadeDealerPercentage() {
+  var dealerPercentage = document.getElementById("dealerPercentage").value;
+  dealerPercentage = Number.parseFloat(dealerPercentage);
+  if (dealerPercentage < 10) {
+    alert("A percentagem do revendedor não pode ser inferior a 10%");
+    return false;
+  } else {
+    if (dealerPercentage > 50) {
+      alert("A percentagem do revendedor não pode ser superior a 50%");
+      return false;
+    }
+  }
+  return true;
+}
+
+function cleanAlert() {
+  if (!validateName()) {
+  }
 }
