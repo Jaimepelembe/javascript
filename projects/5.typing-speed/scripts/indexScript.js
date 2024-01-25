c97c38b3e263059fde7688e415ddace2126b5d45
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2024-01-25 20:30:55 +0200

    I added the project of typing game on my site of Github.

--- projects/5.typing-speed/scripts/indexScript.js
+++
@@ -1,6 +0,0 @@
-var timeSelect = document.getElementById("timeSelect").value;
-var buttonPlay = document.getElementById("buttonPlay");
-
-buttonPlay.addEventListener("click", function (event) {
-  window.location.href = "game.html";
-});
